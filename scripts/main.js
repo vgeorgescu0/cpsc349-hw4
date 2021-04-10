e19a0631a40ce18d2b5b6073c08b6b9f64c801a8
vgeorgescu0 <71309994+vgeorgescu0@users.noreply.github.com>
2021-04-10 00:12:56 -0700

    Update to add and correct FireStore
    Added and correctly connected FireStore into coffeerun app.

--- scripts/main.js
+++ scripts/main.js
@@ -2,24 +2,35 @@
     'use strict';
     var FORM_SELECTOR = '[data-coffee-order="form"]';
     var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
-    var SERVER_URL = 'http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders';
+    // var SERVER_URL = 'https://co.audstanley.com/coffeeorders';
     var App = window.App;
+    
     var Truck = App.Truck;
-    var DataStore = App.DataStore;
-    var RemoteDataStore = App.RemoteDataStore;
+    // var DataStore = App.DataStore;
+    // var RemoteDataStore = App.RemoteDataStore;
+    var FireBaseDataStore = App.FireBaseDataStore;
     var FormHandler = App.FormHandler;
     var Validation = App.Validation;
     var CheckList = App.CheckList;
-    var remoteDS = new RemoteDataStore(SERVER_URL);
+
+    // var remoteDS = new RemoteDataStore(SERVER_URL);
+    var remoteDS = new FireBaseDataStore();
+
     var myTruck = new Truck('ncc-1701', remoteDS);
     window.myTruck = myTruck;
     var checkList = new CheckList(CHECKLIST_SELECTOR);
     checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
     var formHandler = new FormHandler(FORM_SELECTOR);
 
+    // formHandler.addSubmitHandler(function (data) {
+    //     myTruck.createOrder.call(myTruck, data);
+    //     checkList.addRow.call(checkList, data);
+    // });
+
     formHandler.addSubmitHandler(function (data) {
-        myTruck.createOrder.call(myTruck, data);
-        checkList.addRow.call(checkList, data);
+        return myTruck.createOrder.call(myTruck, data).then(() => {
+            checkList.addRow.call(checkList, data);
+        });
     });
 
     formHandler.addInputHandler(Validation.isCompanyEmail);
--- scripts/remotedatastore.js
+++ scripts/remotedatastore.js
@@ -1,42 +1,48 @@
 (function (window) {
     'use strict';
+
     var App = window.App || {};
     var $ = window.jQuery;
 
-    function RemoteDataStore(url) {
-        if (!url) {
-            throw new Error('No remote URL supplied.');
+    var form = document.querySelector('.myForm');
+
+    class RemoteDataStore {
+        constructor(url) {
+            console.log('running the DataStore function');
+    
+            if (!url) {
+                throw new Error('No remote URL supplied.');
+            }
+            this.serverURL = url;
         }
-
-        this.serverUrl = url;
+    
+        // helps with POST and PUT requests for NON-Firebase requests
+        post_helper(type, url, val) {
+            return $.ajax({ type: type, url: url, contentType: 'application/json',
+                data: JSON.stringify(val),
+                success: function (response) {
+                    console.log('function returned: ' + JSON.stringify(response));
+              }
+            });
+        }
+    
+        helper(type, url, cb) {
+            return $.ajax({ type: type, url: url, contentType: 'application/json',
+                success: function (response) {
+                    console.log('function returned: ' + JSON.stringify(response));
+                    if (cb !== undefined) { cb(response); }
+                }
+            });
+        }
+    
+        add(key, val) { return this.post_helper('POST',   this.serverURL, val);             }
+        get(key, cb)  { return this.helper     ('GET',    this.serverURL + '/' + key, cb);  }
+        getAll(cb)    { return this.helper     ('GET',    this.serverURL, cb);              }
+        put(key, val) { return this.post_helper('PUT',    this.serverURL + '/' + key, val); }
+        remove(key)   { return this.helper     ('DELETE', this.serverURL + '/' + key);      }
     }
-
-    RemoteDataStore.prototype.add = function (key, val) {
-        $.post(this.serverUrl, val, function (serverResponse) {
-            console.log(serverResponse);
-        });
-    };
-
-    RemoteDataStore.prototype.getAll = function (cb) {
-        $.get(this.serverUrl, function (serverResponse) {
-            console.log(serverResponse);
-            cb(serverResponse);
-        });
-    };
-
-    RemoteDataStore.prototype.get = function (key, cb) {
-        $.get(this.serverUrl + '/' + key, function (serverResponse) {
-            console.log(serverResponse);
-            cb(serverResponse);
-        });
-    };
-
-    RemoteDataStore.prototype.remove = function (key) {
-        $.ajax(this.serverUrl + '/' + key, {
-            type: 'DELETE'
-        });
-    };
 
     App.RemoteDataStore = RemoteDataStore;
     window.App = App;
+
 })(window);
